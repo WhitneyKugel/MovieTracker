08b98b18904fd3cda6ee9268508513b9a74294f7
Whitney Kugel <kugel.whitney@gmail.com>
2023-11-24 16:14:38 -0600

    Continue migration to REST

--- src/main/java/service/MovieService.java
+++ src/main/java/service/MovieService.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Service
 public interface MovieService {
 
-    List<Movie> getAllMovies() throws MovieDaoException;
+    List<Movie> getAllMovies();
 
     Movie getMovieByTitle(String title);
 }
